e90f9ffb45495ebde39fe1b7b2e00addac322d5b
p4535992 <tentimarco0@gmail.com>
2023-07-06 22:23:57 +0200

    u

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,18 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import resolve from "@rollup/plugin-node-resolve"; // This resolves NPM modules from node_modules.
 import preprocess from "svelte-preprocess";
-import { postcssConfig, terserConfig } from "@typhonjs-fvtt/runtime/rollup";
+import {
+  postcssConfig,
+  terserConfig,
+  typhonjsRuntime
+} from '@typhonjs-fvtt/runtime/rollup';
 import { viteZip } from 'vite-plugin-zip-file';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+import cleanPlugin from 'vite-plugin-clean';
 import { normalizePath } from 'vite';
 import path from 'path';
+import vue from '@vitejs/plugin-vue'
+// import sassDts from 'vite-plugin-sass-dts'
 // import hbsPlugin from './build/hbsPlugin';
 // import translationPlugin from './build/translationPlugin';
 
@@ -14,7 +21,8 @@ import path from 'path';
 
 // For convenience, you just need to modify the package ID below as it is used to fill in default proxy settings for
 // the dev server.
-const s_PACKAGE_ID = "modules/token-factions";
+const s_MODULE_ID = "token-factions";
+const s_PACKAGE_ID = "modules/"+s_MODULE_ID;
 const s_ENTRY_JAVASCRIPT = "main.mjs";
 
 // A short additional string to add to Svelte CSS hash values to make yours unique. This reduces the amount of
@@ -24,6 +32,11 @@ const s_SVELTE_HASH_ID = "ese";
 
 const s_COMPRESS = false; // Set to true to compress the module bundle.
 const s_SOURCEMAPS = true; // Generate sourcemaps for the bundle (recommended).
+
+// EXPERIMENTAL: Set to true to enable linking against the TyphonJS Runtime Library module.
+// You must add a Foundry module dependency on the `typhonjs` Foundry package or manually install it in Foundry from:
+// https://github.com/typhonjs-fvtt-lib/typhonjs/releases/latest/download/module.json
+const s_TYPHONJS_MODULE_LIB = false;
 
 // Used in bundling particularly during development. If you npm-link packages to your project add them here.
 const s_RESOLVE_CONFIG = {
@@ -42,7 +55,8 @@ export default () => {
     resolve: { conditions: ["import", "browser"] },
 
     esbuild: {
-      target: ["es2022"],
+      target: ['es2022', 'chrome100'],
+      keepNames: true   // Note: doesn't seem to work.
     },
 
     css: {
@@ -75,12 +89,12 @@ export default () => {
       },
     },
     build: {
-      outDir: normalizePath( path.resolve(__dirname, './dist')), // __dirname,
+      outDir: normalizePath( path.resolve(__dirname, `./dist/${s_MODULE_ID}`)), // __dirname,
       emptyOutDir: false,
       sourcemap: s_SOURCEMAPS,
       brotliSize: true,
       minify: s_COMPRESS ? "terser" : false,
-      target: ["es2022"],
+      target: ['es2022', 'chrome100'],
       terserOptions: s_COMPRESS ? { ...terserConfig(), ecma: 2022 } : void 0,
       lib: {
         entry: "./" + s_ENTRY_JAVASCRIPT,
@@ -89,46 +103,43 @@ export default () => {
       },
     },
 
-    // package: {
-    //     // outDir: normalizePath(path.resolve(__dirname, './package')),
-    //     folderPath: normalizePath(path.resolve(__dirname, './dist')),
-    //     outPath: normalizePath(path.resolve(__dirname, './package')),
-    //     zipName: 'module.zip',
-    // }
-
     plugins: [
-    //   hbsPlugin(),
-    //   translationPlugin('./src/lang', './dist/lang'),
-       viteZip({
-        folderPath: normalizePath(path.resolve(__dirname, './dist')),
-        outPath: normalizePath(path.resolve(__dirname, './package')),
-        zipName: 'module.zip'
-      }),
+      //   vue(),
+      //   hbsPlugin(),
+      //   translationPlugin('./src/lang', './dist/lang'),
       viteStaticCopy({
         targets: [
           {
             src: normalizePath(path.resolve(__dirname, './src/assets')) + '/[!.]*', // 1️
-            dest: normalizePath(path.resolve(__dirname, './dist/assets')), // 2️
+            dest: normalizePath(path.resolve(__dirname, `./dist/${s_MODULE_ID}/assets`)), // 2️
+          },
+          {
+            src: normalizePath(path.resolve(__dirname, './src/images')) + '/[!.]*', // 1️
+            dest: normalizePath(path.resolve(__dirname, `./dist/${s_MODULE_ID}/images`)), // 2️
+          },
+          {
+            src: normalizePath(path.resolve(__dirname, './src/icons')) + '/[!.]*', // 1️
+            dest: normalizePath(path.resolve(__dirname, `./dist/${s_MODULE_ID}/icons`)), // 2️
           },
           {
             src: normalizePath(path.resolve(__dirname, './src/templates')) + '/[!.]*', // 1️
-            dest: normalizePath(path.resolve(__dirname, './dist/templates')), // 2️
+            dest: normalizePath(path.resolve(__dirname, `./dist/${s_MODULE_ID}/templates`)), // 2️
           },
           {
             src: normalizePath(path.resolve(__dirname, './src/lang')) + '/[!.]*', // 1️
-            dest: normalizePath(path.resolve(__dirname, './dist/lang')), // 2️
+            dest: normalizePath(path.resolve(__dirname, `./dist/${s_MODULE_ID}/lang`)), // 2️
           },
           {
             src: normalizePath(path.resolve(__dirname, './src/languages')) + '/[!.]*', // 1️
-            dest: normalizePath(path.resolve(__dirname, './dist/language')), // 2️
+            dest: normalizePath(path.resolve(__dirname, `./dist/${s_MODULE_ID}/languages`)), // 2️
           },
           {
             src: normalizePath(path.resolve(__dirname, './src/styles')) + '/[!.]*', // 1️
-            dest: normalizePath(path.resolve(__dirname, './dist/styles')), // 2️
+            dest: normalizePath(path.resolve(__dirname, `./dist/${s_MODULE_ID}/styles`)), // 2️
           },
           {
             src: normalizePath(path.resolve(__dirname, './src/module.json')), // 1️
-            dest: normalizePath(path.resolve(__dirname, './dist')), // 2️
+            dest: normalizePath(path.resolve(__dirname, `./dist/${s_MODULE_ID}/`)), // 2️
           },
         ],
       }),
@@ -141,9 +152,29 @@ export default () => {
           cssHash: ({ hash, css }) => `svelte-${s_SVELTE_HASH_ID}-${hash(css)}`,
         },
         preprocess: preprocess(),
+        onwarn: (warning, handler) => {
+          // Suppress `a11y-missing-attribute` for missing href in <a> links.
+          // Foundry doesn't follow accessibility rules.
+          if (warning.message.includes(`<a> element should have an href attribute`)) {
+            return;
+          }
+          
+          // Let Rollup handle all other warnings normally.
+          handler(warning);
+        },
       }),
+      resolve(s_RESOLVE_CONFIG), // Necessary when bundling npm-linked packages.     
+      
+      // When s_TYPHONJS_MODULE_LIB is true transpile against the Foundry module version of TRL.
+      s_TYPHONJS_MODULE_LIB && typhonjsRuntime(),
 
-      resolve(s_RESOLVE_CONFIG), // Necessary when bundling npm-linked packages.
-    ],
+      viteZip({
+        folderPath: path.resolve(__dirname, `./dist/${s_MODULE_ID}`),
+        outPath: path.resolve(__dirname, './package'),
+        zipName: 'module.zip',
+        enabled: true
+      }),
+      cleanPlugin()
+    ]
   };
 };
