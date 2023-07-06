1cab9e9f1a65f19ea038fc392e98ded3495a0b7c
p4535992 <tentimarco0@gmail.com>
2023-07-06 13:39:39 +0200

    beta v11

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,34 +1,35 @@
 module.exports = {
-  printWidth: 120,
-  tabWidth: 4,
-  useTabs: true,
-  trailingComma: "none",
-  semi: true,
-  bracketSpacing: true,
-  overrides: [
-      {
-          files: ["*.scss", "*.css"],
-          options: {
-              requirePragma: false,
-              parser: "scss",
-          },
-      },
-      {
-          files: "*.html",
-          options: {
-              requirePragma: false,
-              parser: "html",
-              htmlWhitespaceSensitivity: "ignore",
-          },
-      },
-      {
-        files: "*.hbs",
-        options: {
-          requirePragma: false,
-          parser: "glimmer",
-          singleQuote: false,
-          htmlWhitespaceSensitivity: "ignore",
+    tabWidth: 2,
+    useTabs: false,
+    singleQuote: false,
+    trailingComma: "es5",
+    semi: true,
+    printWidth: 120,
+    bracketSpacing: true,
+    overrides: [
+        {
+            files: ["*.scss", "*.css"],
+            options: {
+                requirePragma: false,
+                parser: "scss",
+            },
         }
-      }
-  ],
+        // {
+        //     files: "*.html",
+        //     options: {
+        //         requirePragma: false,
+        //         parser: "html",
+        //         htmlWhitespaceSensitivity: "ignore",
+        //     },
+        // },
+        // {
+        //     files: "*.hbs",
+        //     options: {
+        //     requirePragma: false,
+        //     parser: "glimmer",
+        //     singleQuote: false,
+        //     htmlWhitespaceSensitivity: "ignore",
+        //     }
+        // }
+    ],
 };
